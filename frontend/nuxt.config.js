3702653afa0127f678f247abbad9c0efed55e451
tuna-date <anhtuhspk61@gmail.com>
2021-11-15 11:53:05 +0700

    update page meta data

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - frontend',
-    title: 'frontend',
+    titleTemplate: '%s - Coupons management',
+    title: 'Cobenefito',
     htmlAttrs: {
       lang: 'en'
     },
@@ -18,7 +18,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/coupon.png' }
     ]
   },
 
