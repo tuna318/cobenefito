19e27f98266cf0927943714b7dd359115498a794
tuna-date <anhtuhspk61@gmail.com>
2021-11-29 23:49:28 +0700

    rspec for model association creation

--- backend/spec/models/coupon_transaction_spec.rb
+++ backend/spec/models/coupon_transaction_spec.rb
@@ -9,4 +9,28 @@ RSpec.describe CouponTransaction, type: :model do
   describe "validations" do
     it { should validate_presence_of(:price) }
   end
+
+  describe "association on creation" do
+    let(:space) { build_stubbed(:space) }
+    let(:coupon) { build_stubbed(:coupon) }
+    subject { coupon_transaction.save! }
+
+    context "full association" do
+      let(:coupon_transaction) { build(:coupon_transaction, space: space, coupon: coupon) }
+
+      it { expect { subject }.not_to raise_error }
+    end
+
+    context "lack of space association" do
+      let(:coupon_transaction) { build(:coupon_transaction, space: nil, coupon: coupon) }
+
+      it { expect { subject }.to raise_error(ActiveRecord::RecordInvalid) }
+    end
+
+    context "lack of coupon association" do
+      let(:coupon_transaction) { build(:coupon_transaction, space: space, coupon: nil) }
+
+      it { expect { subject }.to raise_error(ActiveRecord::RecordInvalid) }
+    end
+  end
 end
--- backend/spec/models/spaces_user_spec.rb
+++ backend/spec/models/spaces_user_spec.rb
@@ -10,12 +10,32 @@ RSpec.describe SpacesUser, type: :model do
     it { should validate_presence_of(:username) }
     it { should have_secure_password(:user_password) }
     it { should validate_presence_of(:user_password_digest) }
-    it { should define_enum_for(:user_role).with([:manager, :employee]) }
+    it { should define_enum_for(:user_role).with_values([:manager, :employee]) }
     it { should allow_value(nil).for(:user_claimable_points) }
     it { should allow_value(nil).for(:user_reward_points) }
   end
 
-  describe "on saving" do
-    it "test saving"
+  describe "association on creation" do
+    let(:user) { build_stubbed(:user) }
+    let(:space) { build_stubbed(:space) }
+    subject { spaces_user.save! }
+
+    context "full association" do
+      let(:spaces_user) { build(:employee, user: user, space: space) }
+
+      it { expect { subject }.not_to raise_error }
+    end
+
+    context "lack of user association" do
+      let(:spaces_user) { build(:employee, user: nil, space: space) }
+
+      it { expect { subject }.to raise_error(ActiveRecord::RecordInvalid) }
+    end
+
+    context "lack of space association" do
+      let(:spaces_user) { build(:employee, user: user, space: nil) }
+
+      it { expect { subject }.to raise_error(ActiveRecord::RecordInvalid) }
+    end
   end
 end
