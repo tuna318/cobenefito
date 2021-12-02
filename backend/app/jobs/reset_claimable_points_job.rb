c0fbddc65895e078b65ca669392927ca66f4b7dd
tuna-date <anhtuhspk61@gmail.com>
2021-12-02 11:48:45 +0700

    add rubocop ci

--- backend/app/jobs/reset_claimable_points_job.rb
+++ backend/app/jobs/reset_claimable_points_job.rb
@@ -1,7 +1,7 @@
 class ResetClaimablePointsJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(*_args)
     SpacesUser.update_all(user_claimable_points: 100)
     # Do something later
   end
